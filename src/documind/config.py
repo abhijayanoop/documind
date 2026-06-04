b59d58bb05da5dc9d9ae6211b8d91cd474a9947e
abhijayanoop <abhijayanoop007@gmail.com>
2026-06-04 11:49:15 +0530

    initial commit

---
+++ src/documind/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict;
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    database_url: str
+    embedding_dimension: int = 1536
+
+settings = Settings()
